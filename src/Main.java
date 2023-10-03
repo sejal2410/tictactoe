e0cdca8c57d8937b5767e027560e6fa81a0af14e
sdivekar <SDivekar@campsystems.com>
2023-10-02 20:43:25 -0400

    few classes added

--- src/Main.java
+++ src/Main.java
@@ -12,13 +12,13 @@
  * 2. A team can start a new game (either of the player can start the game)
  * 3. Player can resume the game or terminate the game after login in
  * 4. If the game is terminate by any player, the game ends and other player is notified about it
- * 5. Player can only make alternate moves
+ * 5. [done] Player can only make alternate moves
  * 6. Notify the player that they can make move now as other player has played his chance
  * 7. Maintain instances for each active game in the env
- * 8. Allows only valid moves
- * 9. maintain scores for player
+ * 8. [done]Allows only valid moves
+ * 9. [done]maintain scores for player
  * 10. maintain number of games played by each player
- * 11. get top 10 scores for any instance for each zonal consideration region,city, country.
+ * 11. [done]get top 10 scores for any instance for each zonal consideration region,city, country.
  * 12. match each player with another active player with priority to proximity
  *
  *
@@ -30,15 +30,83 @@
  * 3. board
  * 4. tictactoe
  * 5. scoreboard
- * 6.
+ *
+ * Functionalities:
+ *
+ *
+ *
  */
 
 
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 class TicTacToe{
+
 }
+
+class ScoreBoard{
+    PriorityQueue<Player> topPlayers;
+    CountryName country;
+    HashMap<UUID, Integer>  scores;
+
+    ScoreBoard(CountryName country){
+        scores = new HashMap<>();
+        this.country = country;
+    }
+
+    void addWinner(Player player){
+        scores.put(player.playerId, scores.getOrDefault(player.playerId,0)+1);
+    }
+    int partition(List<UUID> players, int st, int end){
+        UUID last = players.get(end);
+        int i=st-1, j=st;
+        while(j<=end){
+            UUID player = players.get(j);
+            if(scores.get(player)> scores.get(last)){
+                UUID sub = players.get(st+1);
+                players.set(j,sub);
+                players.set(st+1,player);
+                st++;
+            }
+            j++;
+        }
+        UUID sub = players.get(st+1);
+        players.set(st+1, players.get(end));
+        players.set(end, sub);
+       return st+1;
+    }
+    void QuickSort(List<UUID> players, int st, int end, int k){
+        //int st=0, end = players.size()-1;
+        if(st>end) return;
+        int pos = this.partition(players,st,end);
+        if(pos==k)  return;
+        if(pos<k){
+            st = pos+1;
+        }
+        else end = pos-1;
+        QuickSort(players,st,end,k);
+    }
+    List<UUID> getTopKPlayers(int k){
+       //use quickselect salgo to get top 10 players but it will come in unsorted order
+        //to return sorted result, get the first top 10 then sort them and return
+        List<UUID> ans = new ArrayList<>();
+        List<UUID> players = new ArrayList<>();
+        players.addAll(scores.keySet());
+        QuickSort(players,0, players.size()-1, k);
+        for(int i=0;i<k;i++){
+            ans.add(players.get(i));
+        }
+        return ans;
+    }
+
+}
+
+//class USAScoreBoard extends ScoreBoard{
+//    USAScoreBoard(){
+//        super(CountryName.USA);
+//    }
+//    //can add more region specific function to have different logic for
+//}
 class Game{
     Player player1;
     Player player2;
